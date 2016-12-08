196c6887fd6d8cdfdfad31070fe099e57ce00e73
Jeroen Massar <jeroen@massar.ch>
2016-12-08 21:41:12 +0100

    Split out TriGroupMember to separate file

--- src/lib/group.go
+++ src/lib/group.go
@@ -29,46 +29,9 @@ type TriGroupS struct {
 	Nom_enabled     bool   `label:"Nominations Enabled" pfset:"group_admin"`
 }
 
-type TriGroupMember interface {
-	pf.PfGroupMember
-	GetVouchesFor() int
-	GetVouchesBy() int
-	GetVouchesForMe() int
-	GetVouchesByMe() int
-}
-
-type TriGroupMemberS struct {
-	pf.PfGroupMember
-	VouchesFor   int
-	VouchesBy    int
-	VouchesForMe int
-	VouchesByMe  int
-}
-
-func (o *TriGroupMemberS) GetVouchesFor() int {
-	return o.VouchesFor
-}
-
-func (o *TriGroupMemberS) GetVouchesBy() int {
-	return o.VouchesBy
-}
-
-func (o *TriGroupMemberS) GetVouchesForMe() int {
-	return o.VouchesForMe
-}
-
-func (o *TriGroupMemberS) GetVouchesByMe() int {
-	return o.VouchesByMe
-}
-
 /* Don't call directly, use ctx.NewGroup() */
 func NewTriGroup() pf.PfGroup {
 	return &TriGroupS{PfGroup: pf.NewPfGroup()}
-}
-
-func NewTriGroupMember() TriGroupMember {
-	pfg := pf.NewPfGroupMember()
-	return &TriGroupMemberS{PfGroupMember: pfg}
 }
 
 func (grp *TriGroupS) GetVouch_adminonly() bool {
