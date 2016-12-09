5114b40838c50591221ad79f6d463e98bed2ea18
Ben April <bapril@people.ops-trust.net>
2016-12-09 10:50:56 -0500

    go fmt

--- src/lib/group.go
+++ src/lib/group.go
@@ -345,7 +345,7 @@ func group_add(ctx pf.PfCtx, args []string) (err error) {
 func group_member_nominate(ctx pf.PfCtx, args []string) (err error) {
 	grp := ctx.SelectedGroup()
 	user := args[1]
-	err = ctx.SelectUser(user,pf.PERM_USER_NOMINATE)
+	err = ctx.SelectUser(user, pf.PERM_USER_NOMINATE)
 	if err != nil {
 		err = errors.New("User selection failed")
 		return
